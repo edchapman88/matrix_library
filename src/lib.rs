6dbd51650f34d41cc331e846c24ef42c376e10ee
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-05-04 16:37:02 +0100

    matmul benchmark added

--- src/lib.rs
+++ src/lib.rs
@@ -179,8 +179,8 @@ impl Mul for Matrix<f64> {
 #[cfg(test)]
 mod tests {
     use std::vec;
-
     use super::*;
+    use std::time::Instant;
 
     #[test]
     fn new_matrix() {
@@ -250,5 +250,45 @@ mod tests {
         let a = Matrix::new(vec![vec![1,2,3],vec![4,5,6]]);
         assert_eq!(a.to_vec(),
         vec![vec![1,2,3],vec![4,5,6]]);
+    }
+
+    #[test]
+    fn benchmark_matmul() {
+        let n_itr = 100;
+
+        fn time_matmul(a:Matrix<f64>,b:Matrix<f64>,n_itr:u32) -> (f64,f64) {
+            let mut times = Vec::new();
+            for _ in 0..n_itr {  
+                let now = Instant::now();
+                {
+                    a.matmul(&b);
+                }
+                let elapsed = now.elapsed();
+                times.push(elapsed.as_secs_f64() * 1000.0);  // s -> ms
+            }
+            // println!("{:?}",times);
+            let times_sqrt: Vec<f64> = times.iter().map(|x| x.powf(2.0)).collect();
+    
+            let x_sum: f64 = times.iter().sum();
+            let x_sqrt_sum: f64 = times_sqrt.iter().sum();
+            let x_bar: f64 = x_sum / f64::from(n_itr);
+    
+            let var = ( x_sqrt_sum / f64::from(n_itr) ) - x_bar.powf(2.0);
+            (x_bar,var.sqrt())
+        }
+
+        
+        let a = Matrix::new(vec![vec![1.0,2.0,3.0],vec![4.0,5.0,6.0]]);
+        let b = Matrix::new(vec![vec![1.0,2.0],vec![3.0,4.0],vec![5.0,6.0]]);
+        
+        let (mut x_bar,mut stnd) = time_matmul(a, b, n_itr);
+        println!("small matmul: {:.5} ms ({:.5} ms std)",x_bar,stnd );
+
+        let dim_size = 100;
+        let a = Matrix::new(vec![vec![5.0; dim_size]; dim_size]);
+        let b = Matrix::new(vec![vec![5.0; dim_size]; dim_size]);
+        
+        let (mut x_bar,mut stnd) = time_matmul(a, b, n_itr);
+        println!("({},{}) matmul: {:.2} ms ({:.2} ms std)",dim_size,dim_size,x_bar,stnd );
     }
 }
