114b258e420f77c19d208d2f0151b22e74852f5d
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-05-30 13:45:59 +0100

    dimsum and softmax

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use std::fmt::Debug;
 use std::fmt::Display;
 use std::iter::zip;
 use std::ops::AddAssign;
+use std::ops::Div;
+use std::ops::Range;
 use std::ops::{Add, Mul};
 
 use math_utils::{Exp, Pow};
@@ -158,6 +160,35 @@ impl<T: Exp + Clone> Exp for Matrix<T> {
     }
 }
 
+impl<T: Exp + Clone + AddAssign + Add<Output = T> + Div<Output = T>> Matrix<T> {
+    pub fn softmax(&self, dim: usize) -> Matrix<T> {
+        let mut res = self.clone();
+        let e = self.clone().exp();
+        if dim == 0 {
+            // find column-wise sums
+            let sums = e.dim_sum(0);
+            for j in 0..self.shape().0 {
+                for i in 0..self.shape().1 {
+                    *res.at_mut((j, i)).unwrap() =
+                        e.at((j, i)).unwrap().clone() / sums.at((0, i)).unwrap().clone()
+                }
+            }
+        } else if dim == 1 {
+            // find row-wise sums
+            let sums = e.dim_sum(1);
+            for j in 0..self.shape().0 {
+                for i in 0..self.shape().1 {
+                    *res.at_mut((j, i)).unwrap() =
+                        e.at((j, i)).unwrap().clone() / sums.at((j, 0)).unwrap().clone()
+                }
+            }
+        } else {
+            panic!("Only 2D matricies are supported. softmax dim must be 0 or 1.")
+        }
+        res
+    }
+}
+
 impl<T: Pow + Clone> Pow<T> for Matrix<T> {
     fn pow(mut self, exp: T) -> Matrix<T> {
         for row in self.values.iter_mut() {
@@ -242,10 +273,21 @@ impl<T: Clone + Mul<Output = T> + AddAssign> Matrix<T> {
     }
 }
 
-impl<T: Add<Output = T>> Add for Matrix<T> {
+impl<T: Add<Output = T> + Clone> Add for Matrix<T> {
     type Output = Matrix<T>;
     fn add(mut self, mut rhs: Self) -> Self::Output {
         let mut res = VecDeque::new();
+        if self.shape() != rhs.shape() {
+            if rhs.shape().1 == 1 && self.shape().0 == rhs.shape().0 {
+                let mut rhs_broadcast = Vec::new();
+                for _ in 0..self.shape().1 {
+                    rhs_broadcast.push(rhs.clone().transpose().values.pop_back().unwrap().into());
+                }
+                rhs = Matrix::from_vecs(rhs_broadcast).transpose();
+            } else {
+                panic!("Tried matrix addition with shapes that could not broadcast.")
+            }
+        }
         for _ in 0..self.nrows {
             for i in 0..self.ncols {
                 if i == 0 {
@@ -262,6 +304,35 @@ impl<T: Add<Output = T>> Add for Matrix<T> {
             }
         }
         Matrix::new(res)
+    }
+}
+
+impl<T: Add<Output = T> + AddAssign + Clone> Matrix<T> {
+    pub fn dim_sum(&self, dim: usize) -> Matrix<T> {
+        let mut res = Vec::new();
+        if dim == 0 {
+            // column-wise sum
+            for i in 0..self.shape().1 {
+                let mut column_sum = self.at((0, i)).unwrap().clone();
+                for j in 1..self.shape().0 {
+                    column_sum += self.at((j, i)).unwrap().clone()
+                }
+                res.push(column_sum);
+            }
+            return Self::from_vecs(vec![res]);
+        }
+        if dim == 1 {
+            // row-wise sum
+            for j in 0..self.shape().0 {
+                let mut row_sum = self.at((j, 0)).unwrap().clone();
+                for i in 1..self.shape().1 {
+                    row_sum += self.at((j, i)).unwrap().clone()
+                }
+                res.push(row_sum);
+            }
+            return Self::from_vecs(vec![res]).transpose();
+        }
+        panic!("Only 2D matricies are supported. softmax dim must be 0 or 1.")
     }
 }
 
@@ -350,6 +421,73 @@ mod tests {
     }
 
     #[test]
+    fn add_broadcast() {
+        let a = Matrix::new(VecDeque::from([
+            VecDeque::from([1, 2, 3]),
+            VecDeque::from([4, 5, 6]),
+        ]));
+        let b = Matrix::new(VecDeque::from([VecDeque::from([1]), VecDeque::from([4])]));
+        let ans = Matrix::new(VecDeque::from([
+            VecDeque::from([2, 3, 4]),
+            VecDeque::from([8, 9, 10]),
+        ]));
+        assert_eq!(a + b, ans);
+    }
+
+    #[test]
+    fn columnwise_sum() {
+        let a = Matrix::new(VecDeque::from([
+            VecDeque::from([1, 2, 3]),
+            VecDeque::from([4, 5, 6]),
+        ]));
+        let ans = Matrix::new(VecDeque::from([VecDeque::from([5, 7, 9])]));
+        assert_eq!(a.dim_sum(0), ans);
+    }
+
+    #[test]
+    fn columnwise_softmax() {
+        let a = Matrix::new(VecDeque::from([
+            VecDeque::from([1.0, 5.0]),
+            VecDeque::from([4.0, 5.0]),
+        ]));
+        let ans = Matrix::new(VecDeque::from([
+            VecDeque::from([0.0474, 0.5]),
+            VecDeque::from([0.9526, 0.5]),
+        ]));
+        let t1: f64 = *a.softmax(0).at((0, 0)).unwrap();
+        let a1: f64 = *ans.at((0, 0)).unwrap();
+        let t2: f64 = *a.softmax(0).at((1, 0)).unwrap();
+        let a2: f64 = *ans.at((1, 0)).unwrap();
+        let t3: f64 = *a.softmax(0).at((0, 1)).unwrap();
+        let a3: f64 = *ans.at((0, 1)).unwrap();
+        assert_eq!((t1 * 10000.0).round(), (a1 * 10000.0).round());
+        assert_eq!((t2 * 10000.0).round(), (a2 * 10000.0).round());
+        assert_eq!((t3 * 10000.0).round(), (a3 * 10000.0).round());
+    }
+
+    #[test]
+    fn rowwise_softmax() {
+        let a = Matrix::new(VecDeque::from([
+            VecDeque::from([5.0, 5.0]),
+            VecDeque::from([4.0, 1.0]),
+        ]));
+        let ans = Matrix::new(VecDeque::from([
+            VecDeque::from([0.5, 0.5]),
+            VecDeque::from([0.9526, 0.0474]),
+        ]));
+        let sm = a.softmax(1);
+        let t1: f64 = *sm.at((0, 0)).unwrap();
+        let a1: f64 = *ans.at((0, 0)).unwrap();
+        let t2: f64 = *sm.at((1, 0)).unwrap();
+        let a2: f64 = *ans.at((1, 0)).unwrap();
+        let t3: f64 = *sm.at((0, 1)).unwrap();
+        let a3: f64 = *ans.at((0, 1)).unwrap();
+        assert_eq!((t1 * 10000.0).round(), (a1 * 10000.0).round());
+        assert_eq!((t2 * 10000.0).round(), (a2 * 10000.0).round());
+        assert_eq!((t3 * 10000.0).round(), (a3 * 10000.0).round());
+    }
+
+    #[test]
     fn mul_overflow() {
         let a = Matrix::new(VecDeque::from([
             VecDeque::from([1, 2, 3]),
@@ -415,6 +553,7 @@ mod tests {
             VecDeque::from([1, 2, 3]),
             VecDeque::from([4, 5, 6]),
         ]));
+        println!("{}", a);
         assert_eq!(format!("{a}"), "[[1, 2, 3]\n [4, 5, 6]]");
     }
 
